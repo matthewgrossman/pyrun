407e177339a7da91f2ea5bf8e5873417dc089b44
Matthew Grossman <mgrossman@lyft.com>
2017-06-05 15:23:29 -0700

    add more command line tips

--- main.py
+++ main.py
@@ -7,17 +7,17 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Stream-based python shell tool')
 parser.add_argument('-e',
-                    dest='command',
+                    dest='command', required=True,
                     help='Command to run')
 parser.add_argument('-p', '--print-line',
                     dest='print_line', action='store_true',
-                    help='Prints out the line after evaluating the command')
+                    help='Prints out the line after evaluating the COMMAND')
 parser.add_argument('-v', '--var',
                     dest='var', default=PyRun.SUBSITUTION_VAR,
                     help='Name of variable to substitute for stdin')
 parser.add_argument('-i', '--index',
                     dest='index', default=PyRun.SUBSITUTION_INDEX,
-                    help='Name of variable to substitute in current line #')
+                    help='Name of variable to substitute for current line #')
 parser.add_argument('-m', '--modules',
                     dest='modules', default=modules,
                     help='List of modules to import')
